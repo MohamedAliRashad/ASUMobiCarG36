c2258efe4f4292ad63160c36d9a14be312b60f8a
mohamed ali rashad <m.rashadnow@gmail.com>
2018-03-31 21:45:06 +0200

    Update Car_Code_V2.ino

--- Programming/Car_Code_V2/Car_Code_V2.ino
+++ Programming/Car_Code_V2/Car_Code_V2.ino
@@ -4,20 +4,20 @@
 #include <SoftwareSerial.h>
 SoftwareSerial Bluetooth(A3, A4); // RX, TX
 
-#define M1_pinA 5
-#define M1_pinB 9
-#define M2_pinA 11
-#define M2_pinB 10
+#define ML_pinA 5
+#define ML_pinB 9
+#define MR_pinA 11
+#define MR_pinB 10
 #define Near A5
 
 char Data = 'S'; // the data received
 
 void setup()
 {
-  pinMode(M1_pinA, OUTPUT);
-  pinMode(M1_pinB, OUTPUT);
-  pinMode(M2_pinA, OUTPUT);
-  pinMode(M2_pinB, OUTPUT);
+  pinMode(ML_pinA, OUTPUT);
+  pinMode(ML_pinB, OUTPUT);
+  pinMode(MR_pinA, OUTPUT);
+  pinMode(MR_pinB, OUTPUT);
   
   //Initialization of all pin to be LOW
   allpinslow();
@@ -52,32 +52,32 @@ void Command(char Comm)
   switch(Comm)
   {  
   case 'F':    //Upper
-    analogWrite(M1_pinA,100);
-    digitalWrite(M1_pinB,LOW);
+    analogWrite(ML_pinA,100);
+    digitalWrite(ML_pinB,LOW);
     
-    analogWrite(M2_pinA,100);
-    digitalWrite(M2_pinB,LOW);    
+    analogWrite(MR_pinA,100);
+    digitalWrite(MR_pinB,LOW);    
     break;
   case 'G':    //Down
-    digitalWrite(M1_pinA,LOW);
-    analogWrite(M1_pinB,100);
+    digitalWrite(ML_pinA,LOW);
+    analogWrite(ML_pinB,100);
     
-    digitalWrite(M2_pinA,LOW);
-    analogWrite(M2_pinB,100); 
+    digitalWrite(MR_pinA,LOW);
+    analogWrite(MR_pinB,100); 
     break;
   case 'R':    //Right
-    digitalWrite(M1_pinA,LOW);
-    digitalWrite(M1_pinB,LOW);
+    digitalWrite(ML_pinA,LOW);
+    digitalWrite(ML_pinB,LOW);
     
-    analogWrite(M2_pinA,100);
-    digitalWrite(M2_pinB,LOW); 
+    analogWrite(MR_pinA,100);
+    digitalWrite(MR_pinB,LOW); 
     break; 
   case 'L':    //Left
-    analogWrite(M1_pinA,100);
-    digitalWrite(M1_pinB,LOW);
+    analogWrite(ML_pinA,100);
+    digitalWrite(ML_pinB,LOW);
     
-    digitalWrite(M2_pinA,LOW);
-    digitalWrite(M2_pinB,LOW); 
+    digitalWrite(MR_pinA,LOW);
+    digitalWrite(MR_pinB,LOW); 
     break;     
   case 'S':
     allpinslow();
@@ -87,9 +87,9 @@ void Command(char Comm)
 
 void allpinslow()
 {
-  digitalWrite(M1_pinA,LOW);
-  digitalWrite(M1_pinB,LOW);
-  digitalWrite(M2_pinA,LOW);
-  digitalWrite(M2_pinB,LOW);
+  digitalWrite(ML_pinA,LOW);
+  digitalWrite(ML_pinB,LOW);
+  digitalWrite(MR_pinA,LOW);
+  digitalWrite(MR_pinB,LOW);
 }
 
