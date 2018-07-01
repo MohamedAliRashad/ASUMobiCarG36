c4d5158d8150f29584e3278ee23d905c117e7227
MuhammedSamy <36481864+MuhammedSamy@users.noreply.github.com>
2018-07-01 22:29:04 +0200

    Update main_func_car_v2.ino

--- main_func_car_v2.ino
+++ main_func_car_v2.ino
@@ -12,7 +12,7 @@ void loop ()
 
 }
 void move_dist(float x,char y)
-{float no_inter= (x/circum)*gear_teeth;
+{float no_inter= (x/circum_wheel)*gear_teeth;
 switch(y)
  {
 case:'B': backward();break;                 //<=== using car functions for move
@@ -28,8 +28,26 @@ while (true)
       break;
     }             
   }
-  void set_angle(float theta)
+  void set_angle(float theta , char z)
   {
-  
-  }
+  /*
+  float L = 0.5 * wheel_axis * theta ;  // Delta L  lenght of arc which the car move when its angel changes.
+  float no_inter= (L/circum_wheel)*gear_teeth ;
+  switch(z)
+ {
+case:'L': Left();break;                 //<=== using car functions for move for angle clockwise or anti-clockwise
+default: Right();break;
+ }
+ int k=0;
+while (true)
+  {
+    if(digitalRead(photo_out)==1)    //let func is always high unless stop is called
+    k++;
+    if(k>=no_inter)
+    {
+      Stop();
+      break;
+    }             
+   
+  } */
 }
